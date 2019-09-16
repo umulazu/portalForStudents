5a81b4d49b9cd6b4fc4b3f7780103778ceb6a8b6
AndreyRi <umulazu@gmail.com>
2019-09-16 10:56:47 +0300

    NavBar without flex, before working with different pages.

--- client/components/NavBar/components/NavBar.js
+++ client/components/NavBar/components/NavBar.js
@@ -1,28 +1,79 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
 import { useSelector } from "react-redux";
 import classNames from "./scss/NavBar.module.scss";
 import * as selectors from "../selectors";
+import getClassNames from "../../../utilities/getClassnames";
 
 const NavBar = () => {
     const authorized = useSelector(selectors.isAuthorized);
+    const [focusedIndexOfElement, setFocusedIndexOfElement ] = useState(0);
+    const handleClick = useCallback((index) => {
+        setFocusedIndexOfElement(index);
+    }, []);
+
     if (!authorized) {
         return null;
     }
 
-    // const markClasses = getClassNames({
-    //     [classNames["student-name__active-mark_started"]]: isStarted,
+    const rawMenuItems = [
+        "ОФИС",
+        "МОЕ ВРЕМЯ",
+        "МОИ КОНТРАКТЫ",
+        "ЗАМЕТКИ"
+    ];
+    const menuItems = modifyMenuItems(rawMenuItems, focusedIndexOfElement, handleClick);
+
+    // const menuItems = [
+    //     "ОФИС",
+    //     "МОЕ ВРЕМЯ",
+    //     "МОИ КОНТРАКТЫ",
+    //     "ЗАМЕТКИ"
+    // ].map((item, index)=> {
+    //     const classes = getClassNames({
+    //         [classNames["list__list-item"]]: true,
+    //         [classNames["list__list-item_focused"]]:
+    //             focusedIndexOfElement === index,
+    //     });
+    //     return (
+    //         <li className={classes}
+    //             onClick={
+    //                 (e) => handleClick(index, e)
+    //             }
+    //             key={index}
+    //         >
+    //             {item}
+    //         </li>
+    //     );
     // });
 
     return (
         <div className={classNames["nav-bar"]}>
-            <ul>
-                <li>ОФИС</li>
-                <li>МОЕ ВРЕМЯ</li>
-                <li>МОИ КОНТРАКТЫ</li>
-                <li>ЗАМЕТКИ</li>
+            <ul className={classNames["list"]}>
+                {menuItems}
             </ul>
         </div>
     );
 };
 
 export default NavBar;
+
+
+const modifyMenuItems = (rawMenuItems, focusedIndexOfElement, handleClick) => {
+    return rawMenuItems.map((item, index)=> {
+        const classes = getClassNames({
+            [classNames["list__list-item"]]: true,
+            [classNames["list__list-item_focused"]]:
+            focusedIndexOfElement === index,
+        });
+        return (
+            <li className={classes}
+                onClick={
+                    (e) => handleClick(index, e)
+                }
+                key={index}
+            >
+                {item}
+            </li>
+        );
+    });
+};
